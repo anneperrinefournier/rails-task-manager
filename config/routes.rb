6eaa0af3b602e8d999f0d012c4fe1878448a584c
AnnePerrine Fournier <anneperrine.fournier@gmail.com>
2023-11-14 15:48:20 +0100

    fini sauf css

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :tasks
+
+  # #create
+  # get "/tasks/new", to: "tasks#new"
+  # post "/tasks", to: "tasks#create"
+
+  # #read
+  # get "/tasks", to: "tasks#index"
+  # get "/tasks/:id", to: "tasks#show", as: :task
+
+  # #update
+  # get "/tasks/:id/edit", to: "tasks#edit"
+  # patch "/tasks/:id", to: "tasks#update"
+
+  # #delete
+  # delete "/tasks/:id", to: "tasks#destroy"
+
 end
